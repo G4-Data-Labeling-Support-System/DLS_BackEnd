f986fb67e74bba95d8de15cba770d208ccd09917
Trương Minh Nhật <fleeforezz@gmail.com>
2026-02-26 07:56:13 +0700

    Change swagger version

--- src/main/java/com/group4/DLS/config/OpenApiConfig.java
+++ src/main/java/com/group4/DLS/config/OpenApiConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
     info = @Info(
         title = "Data Labeling System (DLS) API",
-        version = "1.0.0",
+        version = "1.0.36",
         description = "Backend API for managing data annotation projects, assignments, datasets, and labeling workflows",
         contact = @Contact(
             name = "Group 4",
