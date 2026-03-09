0dcdcd818d00fdc67af7bdcce6c6612be29b3480
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-09 10:38:47 +0700

    Be task huvu (#113)
    update

--- src/main/java/com/group4/DLS/controllers/DataItemController.java
+++ src/main/java/com/group4/DLS/controllers/DataItemController.java
@@ -1,7 +1,29 @@
 package com.group4.DLS.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.group4.DLS.domain.dto.response.DataItemResponse;
+import com.group4.DLS.services.DataitemService;
 
-@Controller
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/dataitems")
+@RequiredArgsConstructor
 public class DataItemController {
+
+    @Autowired
+    private final DataitemService dataitemService;
+
+    @GetMapping("/datasets/{id}")
+    @PreAuthorize("hasRole('MANAGER')")
+    public List<DataItemResponse> getAllDataset(@PathVariable String id) {
+        return dataitemService.getAllDataitemForDataset(id);
+    }
 }
