e3075c4574095b293bf354a3645532098bff9fa4
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-11 17:26:32 +0700

    HUVU

---
+++ src/main/java/com/group4/DLS/domain/dto/request/AnnotationSubmitionRequest.java
@@ -0,0 +1,12 @@
+package com.group4.DLS.domain.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AnnotationSubmitionRequest {
+}
