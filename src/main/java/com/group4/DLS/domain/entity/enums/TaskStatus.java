9ecd683386933f37e71ded0fe6e44b56210aad4f
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-11 11:43:26 +0700

    Be task huvu (#126)
    update

--- src/main/java/com/group4/DLS/domain/entity/enums/TaskStatus.java
+++ src/main/java/com/group4/DLS/domain/entity/enums/TaskStatus.java
@@ -3,11 +3,11 @@ package com.group4.DLS.domain.entity.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum TaskStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    COMPLETED,
-    ON_HOLD,
-    CANCELLED;
+    NOT_STARTED,// Default status when a task is created
+    IN_PROGRESS,// Indicates that the task is currently being worked on
+    COMPLETED,// Indicates that the task has been finished successfully
+    ON_HOLD,// Indicates that the task is temporarily paused and not currently being worked on
+    CANCELLED;// Indicates that the task has been cancelled and will not be completed
 
     @JsonCreator
     public static TaskStatus fromString(String value) {
