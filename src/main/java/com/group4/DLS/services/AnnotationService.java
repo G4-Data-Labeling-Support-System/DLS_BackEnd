df2d77a9067a67b3f0d0cd900450a817db129c8f
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-11 16:32:39 +0700

    HUVU

--- src/main/java/com/group4/DLS/services/AnnotationService.java
+++ src/main/java/com/group4/DLS/services/AnnotationService.java
@@ -2,8 +2,11 @@ package com.group4.DLS.services;
 
 import com.group4.DLS.domain.entity.Annotation;
 import com.group4.DLS.domain.entity.Dataitem;
+import com.group4.DLS.domain.entity.Label;
 import com.group4.DLS.domain.entity.Task;
+import com.group4.DLS.domain.entity.enums.AnnotationStatus;
 import com.group4.DLS.repositories.AnnotationRepository;
+import com.group4.DLS.repositories.LabelRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
@@ -17,14 +20,19 @@ import java.util.List;
 public class AnnotationService {
 
     AnnotationRepository annotationRepository;
+    LabelRepository labelRepository;
 
     //assign annotation to task
     public void assignAnnotationToTask(Task task, List<Dataitem> dataitems){
         List<Annotation> list = new ArrayList<>();
 
+        List<Label> labels = labelRepository.findByDataset_DatasetId(task.getAssignment().getDataset().getDatasetId());
         for(Dataitem item : dataitems){
             Annotation annotation = new Annotation();
             annotation.setTask(task);
+            annotation.setUser(task.getAssignment().getAssignedTo());
+            annotation.setAnnotationStatus(AnnotationStatus.DRAFT);
+            annotation.setLabels(labels);
             annotation.setDataitem(item);
 
             list.add(annotation);
