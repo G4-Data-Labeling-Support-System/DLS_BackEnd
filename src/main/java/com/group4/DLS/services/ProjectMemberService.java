d09038b6711ae7a63303924f2afec8e9050c3dc8
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-13 11:10:27 +0700

    HUVU

--- src/main/java/com/group4/DLS/services/ProjectMemberService.java
+++ src/main/java/com/group4/DLS/services/ProjectMemberService.java
@@ -1,10 +1,12 @@
 package com.group4.DLS.services;
 
+import com.group4.DLS.domain.dto.response.ProjectMemberResponse;
 import com.group4.DLS.domain.entity.Project;
 import com.group4.DLS.domain.entity.ProjectMember;
 import com.group4.DLS.domain.entity.User;
 import com.group4.DLS.exceptions.AppException;
 import com.group4.DLS.exceptions.enums.ErrorCode;
+import com.group4.DLS.mappers.ProjectMemberMapper;
 import com.group4.DLS.repositories.ProjectMemberRepository;
 import com.group4.DLS.repositories.ProjectRepository;
 import com.group4.DLS.repositories.UserRepository;
@@ -24,28 +26,26 @@ public class ProjectMemberService {
     ProjectMemberRepository projectMemberRepository;
     ProjectRepository projectRepository;
     UserRepository userRepository;
+    ProjectMemberMapper projectMemberMapper;
 
     //List all members of a project
-    public List<ProjectMember> getMembersByProjectId(String projectId) {
+    public List<ProjectMemberResponse> getMembersByProjectId(String projectId) {
         List<ProjectMember> members = projectMemberRepository.findByProject_ProjectId(projectId);
         if (members.isEmpty()) {
             throw new AppException(ErrorCode.PROJECT_MEMBER_NOT_FOUND);
         }
-        return members;
+        return projectMemberMapper.toProjectMemberResponse(members);
     }
 
     //assign member to project
-    public void assignMemberToProject(String projectId, String userId) {
+    public ProjectMember assignMemberToProject(String projectId, String userId) {
         Project project = projectRepository.findById(projectId).orElseThrow(() -> new AppException(ErrorCode.PROJECT_NOT_FOUND));
         User user = userRepository.findById(userId).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
-
-        if (projectMemberRepository.existsByProject_ProjectIdAndUser_UserId(projectId, userId)) {
-            throw new AppException(ErrorCode.PROJECT_MEMBER_ALREADY_EXISTS);
-        }
 
         ProjectMember projectMember = new ProjectMember();
         projectMember.setProject(project);
         projectMember.setUser(user);
         projectMember.setJoinAt(LocalDateTime.now());
+        return projectMemberRepository.save(projectMember);
     }
 }
