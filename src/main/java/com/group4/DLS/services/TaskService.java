d270734f5a455154eeae79384b5d63573741f67b
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-18 23:28:20 +0700

    HUVU

--- src/main/java/com/group4/DLS/services/TaskService.java
+++ src/main/java/com/group4/DLS/services/TaskService.java
@@ -97,6 +97,10 @@ public class TaskService {
         }
 
         for (Task task: tasks){
+            //so sánh số item trong task với số annotation đã submitted + approved
+            //case1: nếu 20 annotation submitted = với số item task có là task đó đang cần review
+            //case2: nếu 10 item approved và 10 item submitted sau khi sửa
+            // nếu có annatation có status là rejected thì không set lại
             if(task.getTaskDataitems().size() == getAnnotationsNotRejected(task).size()){
                 reviewService.createReviews(task);
                 task.setTaskStatus(TaskStatus.IN_PROGRESS);
