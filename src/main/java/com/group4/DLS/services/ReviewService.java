050cfcc3e26ce68f26a396bbf9280e36bb17adaa
Trương Minh Nhật <fleeforezz@gmail.com>
2026-03-18 22:21:52 +0700

    Fix project status in controller

--- src/main/java/com/group4/DLS/services/ReviewService.java
+++ src/main/java/com/group4/DLS/services/ReviewService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.group4.DLS.domain.entity.Review;
 import com.group4.DLS.exceptions.AppException;
 import com.group4.DLS.exceptions.enums.ErrorCode;
 import com.group4.DLS.repositories.AnnotationRepository;
@@ -22,6 +23,10 @@ public class ReviewService {
     // ================= REMOVE REVIEW BY ANNOTATION_ID =================
     @Transactional
     public void removeReviewByAnnotationId(List<String> annotationIds) {
+
+        // Get all reviews for current annotation
+        List<Review> reviews = reviewRepository.findByAnnotation_AnnotationId(annotationIds);
+
         reviewRepository.deleteByAnnotation_AnnotationIdIn(annotationIds);
     }
 }
