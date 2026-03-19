63baf300c39b4b1f93c2abf87e8180e16d9435c1
Trương Minh Nhật <fleeforezz@gmail.com>
2026-03-19 10:37:25 +0700

    Improve deployment script

--- ci/deploy-dev.groovy
+++ ci/deploy-dev.groovy
@@ -3,19 +3,27 @@ def call(config) {
 
     stage('Deploy to Development Server with Dev tag') {
 
-        sshagent(['development-srv']) {
-            sh"""
-                ssh -o StrictHostKeyChecking=no ${config.devServer} \
-                'sudo docker pull ${image} && 
-                
-                sudo docker stop ${config.appName}-dev || true && 
-                sudo docker rm ${config.appName}-dev || true &&
-                
-                sudo docker run -d -p ${config.devPort}:${config.containerPort} \
-                --name ${config.appName}-dev \
-                --restart unless-stopped \
-                ${image}'
-            """
+        withCredentials([
+            string(credentialsId: 'dls-db-password', variable: 'DB_PASSWORD'),
+            string(credentialsId: 'dls-jwt-password', variable: 'JWT_SECRET')
+        ]) {
+            sshagent(['development-srv']) {
+                sh"""
+                    ssh -o StrictHostKeyChecking=no ${config.devServer} \
+                    "sudo docker pull ${image} && 
+                    
+                    sudo docker stop ${config.appName}-dev || true && 
+                    sudo docker rm ${config.appName}-dev || true &&
+                    
+                    sudo docker run -d -p ${config.devPort}:${config.containerPort} \
+                    --name ${config.appName}-dev \
+                    --restart unless-stopped \
+                    -e SPRING_PROFILES_ACTIVE=dev \
+                    -e DB_PASSWORD='${DB_PASSWORD}' \
+                    -e JWT_SECRET='${JWT_SECRET}' \
+                    ${image}"
+                """
+            }
         }
     }
 }
