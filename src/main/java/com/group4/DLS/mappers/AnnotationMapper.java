674e073eb421b0e394c6368c24648b3375d3b714
HUVU2812 <nguyenhuyvu112005@gmail.com>
2026-03-24 16:36:50 +0700

    HUVU

--- src/main/java/com/group4/DLS/mappers/AnnotationMapper.java
+++ src/main/java/com/group4/DLS/mappers/AnnotationMapper.java
@@ -1,11 +1,10 @@
 package com.group4.DLS.mappers;
 
-import com.group4.DLS.domain.dto.request.AnnotationItemRequest;
+
 import com.group4.DLS.domain.dto.response.AnnotationResponse;
 import com.group4.DLS.domain.entity.Annotation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
